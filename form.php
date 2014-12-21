8dadb68dcedf8b0d85a85f9713040ba215a0c537
Dexx <=>
2014-12-21 13:24:18 +0100

    Ajout du CSS sur l'index + Mise en page du resultat du formulaire

--- form.php
+++ form.php
@@ -7,12 +7,14 @@
  */
 ?>
 
-<?php if( $send && $valid ): ?>
-<div>Bienvenue <?php echo $nom; ?> !</div>
+<?php if( $send ): ?>
+    <?php if($valid) : ?><div class="success">Bienvenue <strong><?php echo $nom; ?></strong> !</div>
+    <?php else : ?><div class="error">Nom invalide :/</div>
+    <?php endif; ?>
 <?php endif; ?>
 <form action="" method="post">
     <div class="form-group">
-        <input type="text" name="nom" value="<?php echo $nom; ?>" placeholder="Noël" required />
+        <input type="text" name="nom" value="<?php echo $nom; ?>" placeholder="Votre prénom" required />
         <input type="submit" name="form-submit" value="Envoyer" />
     </div>
 </form>
--- index.php
+++ index.php
@@ -4,7 +4,14 @@
     <meta charset="utf-8" />
     <title>Openclassrooms Activite</title>
     <style type="text/css">
-
+        .main-container{width:80%; margin:0 auto;}
+        form{margin:10px 0}
+        form input[type="text"]{padding:2px 5px}
+        form input[type="submit"]{padding:2px 10px; height:23px; background:#e6e6e6; border:1px solid #adadad; color:#333}
+        .success{background-color:#dff0d8; color:#3c763d; margin:10px 0 0 0; padding:15px; border:1px solid #d6e9c6}
+        form input[type="submit"]:hover{background:#dedede; cursor: pointer}
+        .error{background-color:#f2dede; color:#a94442; margin:10px 0 0 0; padding:15px; border:1px solid #ebccd1}
+    </style>
     </style>
 </head>
 
